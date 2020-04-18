dacf44f57c0d591640473c9b56f1d88de1761831
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2020-04-17 22:12:19 -0300

    0.8

--- atividade02/src/main/java/com/ac1/atividade02/controller/EmployeeController.java
+++ atividade02/src/main/java/com/ac1/atividade02/controller/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.ac1.atividade02.controller;
 
+import javax.validation.Valid;
+
 import com.ac1.atividade02.entity.Employee;
 import com.ac1.atividade02.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,12 +34,16 @@ public class EmployeeController {
     }
 
     @GetMapping("/register")
-    public String registerForm() {
+    public String registerForm(Employee employee) {
         return "registerEmployee";
     }
 
     @PostMapping("/register")
-    public String saveEmployee(@ModelAttribute Employee employee) {
+    public String saveEmployee( @Valid Employee employee, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+        {
+            return "registerError";
+        }
         employeeService.createEmployee(employee);
         return "redirect:/employee/all";
     }
--- atividade02/src/main/java/com/ac1/atividade02/entity/Employee.java
+++ atividade02/src/main/java/com/ac1/atividade02/entity/Employee.java
@@ -1,13 +1,14 @@
 package com.ac1.atividade02.entity;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * Employee
@@ -21,17 +22,21 @@ public class Employee implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name = "name", nullable = false, length = 80)
+    @NotBlank
+    @Column(name = "name", length = 80)
     private String name;
 
-    @Column(name = "role", nullable = false, length = 40)
+    @NotBlank
+    @Column(name = "role", length = 40)
     private String role;
 
-    @Column(name = "salary", nullable = false, precision = 10, scale = 2)
+    @NotNull
+    @Column(name = "salary", precision = 10, scale = 2)
     private double salary;
 
-    //@Column(name = "admission", nullable = true, length = 80)
-    private Date admission;
+    @NotBlank
+    @Column(name = "graduation", length = 40)
+    private String graduation;
 
     public int getId() {
         return id;
@@ -65,17 +70,17 @@ public class Employee implements Serializable {
         this.salary = salary;
     }
 
-    public Date getAdmission() {
-        return admission;
+    public String getGraduation() {
+        return graduation;
     }
 
-    public void setAdmission(Date admission) {
-        this.admission = admission;
+    public void setGraduation(String graduation) {
+        this.graduation = graduation;
     }
 
     @Override
     public String toString() {
-        return "Employee [id=" + id + ", admission="  + admission + ", name=" + name + ", role=" + role
+        return "Employee [id=" + id + ", graduation="  + graduation + ", name=" + name + ", role=" + role
                 + ", salary=" + salary + "]";
     }
     
